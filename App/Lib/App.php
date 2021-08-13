f27a55d5d5079c4ad03cf26afc4833d9aaa77232
Natalia Suarez <nataliasuarez@Natalias-MacBook-Pro.local>
2021-08-13 18:44:34 -0300

    Add routing: Request, Response and Router classes with controller[wip]

---
+++ App/Lib/App.php
@@ -0,0 +1,11 @@
+<?php namespace App\Lib;
+
+class App
+{
+    public static function run()
+    {
+        Logger::enableSystemLogs();
+        $logger = Logger::getInstance();
+        $logger->info('>>>> Testing App Logger');
+    }
+}
