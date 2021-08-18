b7652fa1d50ad7141cb6a988e0aecbec0fd5169e
Natalia Suarez <nataliasuarez@Natalias-MacBook-Pro.local>
2021-08-17 21:38:38 -0300

    Add Render class with base template and auxiliary functions

--- App/Controller/Users.php
+++ App/Controller/Users.php
@@ -5,62 +5,20 @@ namespace App\Controller;
 use App\Model\Users as UsersRepository;
 use App\Lib\Request;
 use App\Lib\Response;
+use App\Lib\Render;
 
 class Users
 {
-
-    function renderBaseTemplate($base_path, $content)
-    {
-        return <<<HTML
-        <link rel="stylesheet" type="text/css" href="$base_path/index.css">
-        <div class="section">
-            <div class="header">
-                <span class="dark-title"><img src="$base_path/src/assets/people_alt_black_24dp.svg"> Users Admin</span>
-                <div class="content">
-                    <a class="link" href="/users">Users list</a>
-                    <a class="link-add" href="/users/new"><img src="$base_path/src/assets/add_circle_outline_black_24dp.svg">ADD NEW USER</a>
-                </div>
-            </div>
-            {$content}
-        <div>
-        HTML;
-    }
-
-    function renderUserItem($user)
-    {
-        if (empty($user)) {
-            return '';
-        }
-        return <<<HTML
-            <tr class="table-row-item">
-                <td>{$user['user_id']}</td>
-                <td>{$user['first_name']}</td>
-                <td>{$user['last_name']}</td>
-                <td>{$user['mail']}</td>
-                <td class="table-cell-button-action">
-                    <a href="/users/update/{$user['user_id']}">
-                        <img class="table-row-button-action" src="../src/assets/edit_black_24dp.svg">
-                    </a>
-                </td>
-                <td class="table-cell-button-action">
-                    <a href="/users/remove/{$user['user_id']}">
-                        <img class="table-row-button-action" src="../src/assets/delete_outline_black_24dp.svg">
-                    </a>
-                </td>
-            </tr>
-        HTML;
-    }
-
     public function all()
     {
         $users = UsersRepository::all();
         $renderUsers = '';
 
         foreach ($users as $user) {
-            $renderUsers = $renderUsers . $this->renderUserItem($user);
+            $renderUsers = $renderUsers . Render::renderUserItem($user);
         }
 
-        echo $this->renderBaseTemplate('../', <<<HTML
+        echo Render::renderBaseTemplate('../', <<<HTML
         <div class="list-content">
                 <div class="form-card" style="width:90%; height: 70vh">
                     <span class="title">
@@ -93,7 +51,7 @@ class Users
             HTML;
         }
 
-        echo $this->renderBaseTemplate('../', <<<HTML
+        echo Render::renderBaseTemplate('../', <<<HTML
         <div>
             {$renderAlert}
         </div>
@@ -136,7 +94,7 @@ class Users
             HTML;
         }
 
-        echo $this->renderBaseTemplate('../../', <<<HTML
+        echo Render::renderBaseTemplate('../../', <<<HTML
             <div>
                 {$renderAlert}
             </div>
@@ -195,7 +153,7 @@ class Users
             HTML;
         }
 
-        echo $this->renderBaseTemplate('../../../', <<<HTML
+        echo Render::renderBaseTemplate('../../../', <<<HTML
             <div>
                 {$renderAlert}
             </div>
